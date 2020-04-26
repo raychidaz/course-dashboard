e065798bf337b4627fdc224643ded4d37cf07950
Ray Chidaz <raychidaz@gmail.com>
2020-04-26 18:24:47 +0100

    configure webpack dev environment

---
+++ webpack.config.dev.js
@@ -0,0 +1,44 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+process.env.NODE_ENV = "developement";
+
+module.exports = {
+  mode: "development",
+  target: "web",
+  devTool: "cheap-module-source-map",
+  entry: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    stats: "minimal",
+    overlay: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false,
+  },
+  plugin: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      favicon: "src/favicon.ico",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
