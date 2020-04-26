2ee9d9eb25712b1177f9a33262a45cbd217b4ad7
Ray Chidaz <raychidaz@gmail.com>
2020-04-26 19:16:57 +0100

    complete dev environment config, transpile: Babel, bundle: Webpack, Lint: ESLint, web server: Webpack, generating index.html: Webpack, loading changes on save: Webpack, one command via npm scripts.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,12 +2,12 @@ const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-process.env.NODE_ENV = "developement";
+process.env.NODE_ENV = "development";
 
 module.exports = {
   mode: "development",
   target: "web",
-  devTool: "cheap-module-source-map",
+  devtool: "cheap-module-source-map",
   entry: "./src/index",
   output: {
     path: path.resolve(__dirname, "build"),
@@ -22,7 +22,7 @@ module.exports = {
     headers: { "Access-Control-Allow-Origin": "*" },
     https: false,
   },
-  plugin: [
+  plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
       favicon: "src/favicon.ico",
@@ -33,7 +33,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ["babel-loader", "eslint-loader"],
       },
       {
         test: /(\.css)$/,
