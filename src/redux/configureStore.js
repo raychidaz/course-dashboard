c3ccc1b7b6cf80a1733ce41dd5bd8f23460dc3df
Ray Chidaz <raychidaz@gmail.com>
2020-05-11 12:23:02 +0100

    Set up production Redux store, set up Webpack, set  up npm scripts, run production build and review Bundle content

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,18 +1,6 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "./reducers";
-
-import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
-import thunk from "redux-thunk";
-
-export default function configureStore(initialState) {
-  const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
-
-  const middlewares = [thunk, reduxImmutableStateInvariant()];
-
-  return createStore(
-    rootReducer,
-    initialState,
-    composeEnhancers(applyMiddleware(...middlewares))
-  );
+// Use CommonJS require below so we can dynamically import during build run-time.
+if (process.env.NODE_ENV === "PRODUCTION") {
+  module.exports = require("./configureStore.prod");
+} else {
+  module.exports = require("./configureStore.dev");
 }
